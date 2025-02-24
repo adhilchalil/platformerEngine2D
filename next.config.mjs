c5fb042debf3f2278bb354dc238ef95d221cc11a
adhilchalil <chaliladhil@gmail.com>
2025-02-24 21:37:32 +0530

    reconfiguring for deployment path accuracy

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  assetPrefix: isProd ? '/platformerEngine2D/' : '',
-  basePath: isProd ? '/platformerEngine2D' : '',
+  basePath: "/platformerEngine2D",
+  base: "/platformerEngine2D",
   output: 'export',
   reactStrictMode: true,
   images: {
