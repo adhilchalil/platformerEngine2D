eda7acd39735695d37891eab1db9511c093e48ec
adhilchalil <chaliladhil@gmail.com>
2025-02-24 20:16:10 +0530

    switching base page to show gravityroom

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  assetPrefix: isProd ? '/platformerEngine2D/' : '',
+  basePath: isProd ? '/platformerEngine2D' : '',
   output: 'export',
   reactStrictMode: true,
   images: {
